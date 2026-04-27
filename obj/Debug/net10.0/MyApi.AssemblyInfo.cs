bef542479cff954d1d2f513bd19b5c5aed18bb7d
HallCraig <craigh@tfg.co.za>
2026-04-27 15:37:31 +0200

    Update

--- obj/Debug/net10.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3fb558efbfd77133ee041994eac71c32b8c1a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34641ef39e19515dfd02f3bbddac1d4f102d23f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
