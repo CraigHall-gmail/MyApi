ab17c663a44d22fe10987d41f0ecfce8df2f5742
HallCraig <craigh@tfg.co.za>
2026-04-28 19:27:25 +0200

    Updated Terraform structure, aligned with managing multple environments

---
+++ infra/modules/app-environment/main.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "this" {
+  name     = var.resource_group
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_log_analytics_workspace" "this" {
+  name                = var.law_name
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = var.tags
+}
+
+resource "azurerm_container_app_environment" "this" {
+  name                       = var.aca_env_name
+  resource_group_name        = azurerm_resource_group.this.name
+  location                   = azurerm_resource_group.this.location
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
+  tags                       = var.tags
+}
