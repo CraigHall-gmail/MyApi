31e9502dab6353ccd790de7115eeeaa7f2d9a058
HallCraig <craigh@tfg.co.za>
2026-04-30 16:39:15 +0200

    STaging and Prod terrafrom

--- infra/staging/terraform.tfvars
+++ infra/staging/terraform.tfvars
@@ -3,6 +3,15 @@ aca_name_env   = "aca-myapi-staging"
 law_name_env   = "law-myapi-staging"
 location       = "southafricanorth"
 
+acr_name           = "acrimagereg"
+acr_resource_group = "Playground"
+app_name           = "myapi"
+
+cpu          = 0.5
+memory       = "1Gi"
+min_replicas = 1
+max_replicas = 5
+
 tags = {
   project     = "myapi"
   environment = "staging"
--- infra/staging/variables.tf
+++ infra/staging/variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group" {
   type        = string
-  description = "myapi-staging"
+  description = "Name of the Azure resource group"
 }
 
 variable "location" {
@@ -11,16 +11,55 @@ variable "location" {
 
 variable "aca_name_env" {
   type        = string
-  description = "aca-myapi"
+  description = "Name of the Azure Container App Environment"
 }
 
 variable "law_name_env" {
   type        = string
-  description = "law-myapi"
+  description = "Name of the Log Analytics Workspace"
 }
 
 variable "tags" {
   type        = map(string)
   description = "Resource tags"
   default     = {}
+}
+
+variable "acr_name" {
+  type        = string
+  description = "Name of the existing Azure Container Registry"
+}
+
+variable "acr_resource_group" {
+  type        = string
+  description = "Resource group containing the ACR"
+}
+
+variable "app_name" {
+  type        = string
+  description = "Name of the Container App"
+}
+
+variable "cpu" {
+  type        = number
+  description = "CPU allocation for the container"
+  default     = 0.5
+}
+
+variable "memory" {
+  type        = string
+  description = "Memory allocation for the container (e.g. '1Gi')"
+  default     = "1Gi"
+}
+
+variable "min_replicas" {
+  type        = number
+  description = "Minimum number of replicas"
+  default     = 1
+}
+
+variable "max_replicas" {
+  type        = number
+  description = "Maximum number of replicas"
+  default     = 10
 }
