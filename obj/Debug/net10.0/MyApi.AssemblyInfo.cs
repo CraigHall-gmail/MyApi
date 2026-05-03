91b21e3e984921f54639d0d09304c3ac056e87da
HallCraig <craigh@tfg.co.za>
2026-05-03 07:19:05 +0200

    Sonar issues

--- obj/Debug/net10.0/MyApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32263aeb4004b7f3808ebc1cfb874503d3125a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e58b75301681a6638eee56da52eb0971a696c3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
